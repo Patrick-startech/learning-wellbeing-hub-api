89db439ba99bfee9082ebbf25baafb8e6aa09988
Patrick-startech <asamoahpatrick1999@gmail.com>
2025-12-29 17:20:29 -0500

    Fix ALLOWED_HOSTS for Render

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS',
+    'localhost,127.0.0.1,learning-wellbeing-hub-api.onrender.com'
+).split(',')
 
 
 # Application definition
